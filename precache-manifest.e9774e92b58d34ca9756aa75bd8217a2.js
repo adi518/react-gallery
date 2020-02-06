dc0eb402a4746f8ce4f5cb50e2650b4ceafcfa24
Adi Sahar <adisahar1@hotmail.com>
2020-02-06 21:31:45 +0200

    Updates

---
+++ precache-manifest.e9774e92b58d34ca9756aa75bd8217a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "064ace4777ac86d7627f455f36975452",
+    "url": "/rapyd/static/media/GitHub_Logo_White.064ace47.png"
+  },
+  {
+    "revision": "0a4dea19a5dfd02021f3",
+    "url": "/rapyd/static/js/runtime~main.0a4dea19.js"
+  },
+  {
+    "revision": "70314be9103b1f4e6f36",
+    "url": "/rapyd/static/js/main.70314be9.chunk.js"
+  },
+  {
+    "revision": "3d1ec81cddabcb8eae5f",
+    "url": "/rapyd/static/js/1.3d1ec81c.chunk.js"
+  },
+  {
+    "revision": "70314be9103b1f4e6f36",
+    "url": "/rapyd/static/css/main.00c9a220.chunk.css"
+  },
+  {
+    "revision": "a9dd2f3900aa0c891d423c370686c0c3",
+    "url": "/rapyd/index.html"
+  }
+];
